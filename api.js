9b576cdbd86b255f4b359fde081b807a2421ffa2
othmaneBakkass <othmanebakkass@gmail.com>
2022-08-30 09:58:27 +0100

    set up the server added response for get requests and wrong urls

---
+++ api.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const { getProducts, getProduct } = require('./controllers/controller');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/api/products' && req.method === 'GET') {
+    getProducts(req, res);
+  } else if (
+    req.url.match(/\/api\/products\/([0-9]+)/) &&
+    req.method === 'GET'
+  ) {
+    const id = req.url.split('/')[3];
+    getProduct(req, res, id);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'route not found' }));
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log('server running');
+});
