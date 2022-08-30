1227d0dbf3cd91e713d9f7c9ce290e08734c2f70
othmaneBakkass <othmanebakkass@gmail.com>
2022-08-30 12:55:48 +0100

    refactored the server function added handlers for post method

--- api.js
+++ api.js
@@ -1,15 +1,24 @@
 const http = require('http');
-const { getProducts, getProduct } = require('./controllers/controller');
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+} = require('./controllers/controller');
+//checks if page exists for a specific product
+const urlForProductPage = /\/api\/products\/([0-9]+)/;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/api/products' && req.method === 'GET') {
-    getProducts(req, res);
-  } else if (
-    req.url.match(/\/api\/products\/([0-9]+)/) &&
-    req.method === 'GET'
-  ) {
-    const id = req.url.split('/')[3];
-    getProduct(req, res, id);
+  if (req.method === 'GET') {
+    if (req.url === '/api/products') {
+      getProducts(req, res);
+    } else if (req.url.match(urlForProductPage)) {
+      const id = req.url.split('/')[3];
+      getProduct(req, res, id);
+    }
+  } else if (req.method === 'POST') {
+    if (req.url === '/api/products') {
+      createProduct(req, res);
+    }
   } else {
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'route not found' }));
