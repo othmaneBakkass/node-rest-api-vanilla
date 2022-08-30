60a59edb4736208f46f3f886a0f80cac5ccc71c4
othmaneBakkass <othmanebakkass@gmail.com>
2022-08-30 10:01:21 +0100

    added functions for return all products return product by id

---
+++ models/model.js
@@ -0,0 +1,16 @@
+const products = require('../data/products.json');
+
+function findAll() {
+  return new Promise((resolve, reject) => {
+    resolve(products);
+  });
+}
+
+function findByID(id) {
+  return new Promise((resolve, reject) => {
+    const product = products.find((p) => p.id === id);
+    resolve(product);
+  });
+}
+
+module.exports = { findAll, findByID };
